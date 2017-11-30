a9b43ed91b6123ff1d435b8d4bab9c3473439d22
markchr98 <mark4@live.dk>
2017-11-30 14:46:29 +0100

    Basic of getting information from api

---
+++ Getting real/Getting real/Controller.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace Getting_real
+{    
+    class Controller
+    {
+        //change ip to local      
+        const string ip = "10.140.67.116";
+        public int RefreshSeconds
+        {
+            get { return (int)TokenRespone["expires_in"]; }            
+        }
+        public string AccessToken
+        {
+            get { return (string)TokenRespone["access_token"]; }
+        }
+
+        public JObject TokenRespone = new JObject();                       
+
+        public void Run()
+        {
+            TokenRespone = GetNewTokenResponse(ip);
+            var start = DateTime.UtcNow;
+            while (true)
+            {                              
+                if (start.AddSeconds((RefreshSeconds-60)) < DateTime.UtcNow)
+                {
+                    TokenRespone = GetNewTokenResponse(ip);
+                    start = DateTime.UtcNow;
+                    Console.WriteLine(AccessToken);
+                }
+                
+            }
+        }
+        static public JObject GetNewTokenResponse(string ip)
+        {
+            using (WebClient client = new WebClient())
+            {                
+                string yourURL = "http://"+ip+":5000/api/Token";
+                string PARM = "grant_type=password&username=Administrator&password=admin";
+                client.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+                JObject result = JObject.Parse(client.UploadString(yourURL, PARM));
+                
+                return (result);
+            }
+        }
+    }
+}
--- Getting real/Getting real/Program.cs
+++ Getting real/Getting real/Program.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace Getting_real
 {
     class Program
-    {
+    {        
         static void Main(string[] args)
         {
+            Controller controller = new Controller();
+            controller.Run();
         }
     }
 }
